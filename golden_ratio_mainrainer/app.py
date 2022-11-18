460f05ac30cc2a751008bdb2ebf9a60ee9c7b657
nerd_minhaz <116950640+minhaz72@users.noreply.github.com>
2022-11-18 09:47:09 +0600

    Add files via upload

---
+++ golden_ratio_mainrainer/app.py
@@ -0,0 +1,7 @@
+def golden_ration_maintainer( n1: int , n2:  int) : 
+    print('  this is a golden ratio maintainer : that maintain your golden ration in your any kind of mesurment ; ')
+    print( f' you mesumesnt is {n1 * 10 } and another mesurment is {n2* 6 }' )
+if __name__ =='__main__': 
+    n1,  n2= int(input('enter the mesurment : ')).split()
+    golden_ration_maintainer(n1, n2) 
+    
